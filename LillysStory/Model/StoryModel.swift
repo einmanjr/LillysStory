fbbc566855d04f3f249e3784d368d40c49f0289d
Michael Einman <einmanjr@gmail.com>
2021-04-16 13:17:30 -0700

    Beginning Lilly's story

---
+++ LillysStory/Model/StoryModel.swift
@@ -0,0 +1,17 @@
+//
+//  StoryModel.swift
+//  LillysStory
+//
+//  Created by Michael Einman on 4/16/21.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
+
